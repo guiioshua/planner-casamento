17d4b8072ebe10c9212c296c6492a5708f46bf94
guiioshua <guilhermeioshua@gmail.com>
2026-02-19 18:12:10 -0300

    dev: testando feat 2

--- backend/src/main/java/com/projeto/dto/GiftRequest.java
+++ backend/src/main/java/com/projeto/dto/GiftRequest.java
@@ -2,14 +2,10 @@ package com.projeto.dto;
 
 import com.projeto.model.GiftStatus;
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class GiftRequest {
 
     @NotBlank
@@ -21,8 +17,84 @@ public class GiftRequest {
 
     private Boolean visible;
 
-    @com.fasterxml.jackson.annotation.JsonProperty("category")
+    @JsonProperty("category")
     private String category;
 
     private GiftStatus status;
+
+    public GiftRequest() {
+        System.out.println("DEBUG: GiftRequest NO-ARGS constructor called");
+    }
+
+    public GiftRequest(String name, String purchaseLink, String imageUrl, Boolean visible, String category,
+            GiftStatus status) {
+        System.out.println("DEBUG: GiftRequest ALL-ARGS constructor called. Category=" + category);
+        this.name = name;
+        this.purchaseLink = purchaseLink;
+        this.imageUrl = imageUrl;
+        this.visible = visible;
+        this.category = category;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPurchaseLink() {
+        return purchaseLink;
+    }
+
+    public void setPurchaseLink(String purchaseLink) {
+        this.purchaseLink = purchaseLink;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Boolean getVisible() {
+        return visible;
+    }
+
+    public void setVisible(Boolean visible) {
+        this.visible = visible;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        System.out.println("DEBUG: GiftRequest.setCategory called with: " + category);
+        this.category = category;
+    }
+
+    public GiftStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(GiftStatus status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "GiftRequest{" +
+                "name='" + name + '\'' +
+                ", purchaseLink='" + purchaseLink + '\'' +
+                ", imageUrl='" + imageUrl + '\'' +
+                ", visible=" + visible +
+                ", category='" + category + '\'' +
+                ", status=" + status +
+                '}';
+    }
 }
